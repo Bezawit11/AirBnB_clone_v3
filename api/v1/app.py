70927571be9cfef6f53f647a309a41b4b11254b4
Bezawit11 <socbeza13@gmail.com>
2023-02-14 11:47:08 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ a script that starts a Flask web application
 from os import getenv
 from api.v1.views import app_views
 from models import storage
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -14,6 +14,12 @@ def tear_down(self):
     """remove the current SQLAlchemy Session"""
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
+
+def page_not_found(e):
 if __name__ == '__main__':
     app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
             port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
