d319d33c27f936aa9fe35634fe09de459003f5b4
Bezawit11 <socbeza13@gmail.com>
2023-02-14 11:53:07 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,8 +15,8 @@ def tear_down(self):
     storage.close()
 
 @app.errorhandler(404)
-@app.route('/nop', strict_slashes=False)
-def page_not_found():
+@app.route('/api/v1/nop', strict_slashes=False)
+def page_not_found(error):
     """returns a JSON-formatted 404 status code response"""
     return jsonify({"error": "Not found"}), 404
 
