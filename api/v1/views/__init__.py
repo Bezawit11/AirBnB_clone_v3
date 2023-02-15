3cfc46052a6b4a0459fbca373314615b64517fdb
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-02-15 05:27:59 +0000

    adding cities, amenities, places, and users

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,11 @@ Views directory
 """
 from flask import Blueprint
 from api.v1.views.index import *
-from api.views.states import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
