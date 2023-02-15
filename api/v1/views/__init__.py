346a483c0f1ce81defbd2039405f79a8268ae382
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-02-15 05:17:24 +0000

    adds the state apij

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,5 +3,8 @@
 Views directory
 """
 from flask import Blueprint
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
+from api.views.states import *
+
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,17 +7,19 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ returns status """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ retrieves the number of each objects by type"""
-    return jsonify({"amenities": storage.count("Amenity"), 
-		    "cities": storage.count("City"), 
-	            "places": storage.count("Place"), 
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
                     "reviews": storage.count("Review"),
-	            "states": storage.count("State"), 
+                    "states": storage.count("State"),
                     "users": storage.count("User")})
