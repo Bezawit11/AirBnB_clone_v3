b012f8e1f7f3cb81b9a3ffe4a0213652de7ff0f6
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-15 19:33:25 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,8 +6,11 @@ from os import getenv
 from api.v1.views import app_views
 from models import storage
 from flask import Flask, Blueprint, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def tear_down(self):
