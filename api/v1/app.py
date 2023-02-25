577fa16c370ac691cd248f827e214a66a19acac4
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-25 13:02:20 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -45,10 +45,13 @@ def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-if __name__ == '__main__':
-    HBNB_API_HOST = getenv('HBNB_API_HOST')
-    HBNB_API_PORT = getenv('HBNB_API_PORT')
-
-    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
-    port = '5000' if not HBNB_API_PORT else HBNB_API_PORT
+if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
+
