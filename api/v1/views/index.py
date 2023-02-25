6800752212920dfb6bd49b8ef688bfc2a8912055
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-25 13:10:29 +0000

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,9 +18,9 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """ Returns the number of each instance type """
-    return jsonify({amenities=storage.count("Amenity"),
+    return jsonify(amenities=storage.count("Amenity"),
                    cities=storage.count("City"),
                    places=storage.count("Place"),
                    reviews=storage.count("Review"),
                    states=storage.count("State"),
-                   users=storage.count("User")})
+                   users=storage.count("User"))
